f0d73401933382bcb06850c732f0e3161d4bd47c
Fabian <fabian.kamp@fu-berlin.de>
2020-03-08 12:24:00 +0100

    Dynamical functional connectivity added

--- Graph_measures/functions.py
+++ Graph_measures/functions.py
@@ -17,8 +17,9 @@ def pearson_corr(M):
             for the remaining variables in M.
         """
     M=np.asarray(M)
-    P=np.array([np.corrcoef((M[i,:], M[j,:]))[0,1] for i in range(M.shape[0]) for j in range(M.shape[0])])
-    return P.reshape(M.shape[0],M.shape[0])
+    n=M.shape[0]
+    P=np.array([np.corrcoef((M[i,:], M[j,:]))[0,1] for i in range(n) for j in range(n)])
+    return P.reshape(n,n)
 
 def partial_corr_inv(M):
     """ Returns the partial linear correlation coefficients between pairs of rows in M.
@@ -33,12 +34,12 @@ def partial_corr_inv(M):
         P[i, j] contains the partial correlation of M[i, :] and M[j, :] controlling
         for the remaining variables in M.
     """
-
-    M=np.asarray(M)
-    inv_M=np.linalg.inv(pearson_corr(M))
+    corr_M=pearson_corr(M)
+    assert np.all(np.linalg.eigvals(corr_M) > 0), "Matrix is not positive definite."    #check if correlation Matrix is positive definite
+    inv_M=np.linalg.inv(corr_M)
     P=np.array([-(inv_M[i,j])/math.sqrt(inv_M[i,i]*inv_M[j,j]) for i in range(inv_M.shape[0]) for j in range(inv_M.shape[0])])
     P=P.reshape(inv_M.shape)        #reshaping the 1x(p^2) dimensional array to a pxp dimensional array
-    np.fill_diagonal(P, 1)  # fill diagonal with ones and return matrix
+    np.fill_diagonal(P, 1)          #fill diagonal with ones and return matrix
     return P
 
 def partial_corr(M):
@@ -72,5 +73,41 @@ def partial_corr(M):
             part_corr[j,i]=corr
     return part_corr
 
-def
+def dynmfc(TimeCourseBold, windowsize=30, stepsize=10, TR=2, method=1, output_windows=False):
+    """Calculate the dynamical functional connectivity using a moving time window.
+    :param: 1) Time course matrix of Bold Signal
+            2) Windowsize in seconds
+            3) Stepsize in seconds
+            4) TR in seconds
+            5) Method:  1 -- pearson correlation
+                        2 -- partial correlation
+    :return: ndim array containing the correlations between the region-wise correlation matrices for each time window
+    """
+    assert int(stepsize) >= int(TR), "Stepsize has to be greater than repetition time"
+    tc=np.asarray(TimeCourseBold)
+    nregions=tc.shape[0]
+    wsize=np.around(float(windowsize)/float(TR))    #converts windowsize seconds to frames of Bold-Signal
+    stpsize=np.around(float(stepsize)/float(TR))    #converts stepsize to Bold-Signal frames
+    frnum=tc.shape[1]   #counts number of Bold-Signal frames
+    ls_corr_mat=[]      #initiate list of correlation matrices
+    strtpoint=0
+    while strtpoint <= frnum-wsize:
+        endpoint=strtpoint+int(wsize)     #endpoint of current slice
+        tc_slice=tc[:, strtpoint:endpoint]
+        if method == 1:
+            ls_corr_mat.append(pearson_corr(tc_slice))   #appends a nregions x nregions dimensional corrmatrix to list
+        elif method == 2:
+            ls_corr_mat.append(partial_corr_inv(tc_slice))   #appends the matrix using partial correlation
+        strtpoint += int(stpsize)
+    excl_fr=frnum-endpoint    #calculating the number of frames not included in the analysis.
+    if excl_fr != 0:
+        print(excl_fr,'Bold-frames not included in analysis.')
+
+    if output_windows: return np.array(ls_corr_mat)         #outputs corrlation matrix for each time window
+
+    fcd=np.empty((len(ls_corr_mat), len(ls_corr_mat)))      #initialize fcd matrix
+    for i in range(len(ls_corr_mat)):
+        for j in range(len(ls_corr_mat)):
+            fcd[i,j]=np.corrcoef(np.array(ls_corr_mat[i]).reshape(1,nregions**2), np.array(ls_corr_mat[j]).reshape(1,nregions**2))[0,1]
+    return fcd
 
