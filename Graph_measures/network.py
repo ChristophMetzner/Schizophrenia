da5a27ccc65b52b517bd41174acb5db6fa860f39
Fabian <fabian.kamp@fu-berlin.de>
2020-03-16 22:43:52 +0100

    Characteristic Path length added

--- Graph_measures/network.py
+++ Graph_measures/network.py
@@ -14,13 +14,14 @@ class network:
         Calculate the degree of each node in the network and saves it as pd.Series
         """
         return self.adj_mat.sum(axis=1)-1
+
     def shortestpath(self):
         """
-        Calculate the shortest path between all nodes in the network using the Dijstrak Algorithm
+        Calculate the shortest path between all nodes in the network using Dijstrak Algorithm
         https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
         """
         inv_adj_mat=self.adj_mat.abs().pow(-1)                                                                          # Inverts adjacency matrix
-        shortestpath_mat=pd.DataFrame(np.zeros(inv_adj_mat.shape), columns=self.nodes, index=self.nodes)                # Initialize
+        shortestpath_df=pd.DataFrame(np.zeros(inv_adj_mat.shape), columns=self.nodes, index=self.nodes)                # Initialize
         counter=0
         for n in self.nodes:
             node_set=pd.DataFrame({'Distance': np.full((len(self.nodes)-counter), np.inf), 'Previous': ['']*(len(self.nodes)-counter)}, index=self.nodes[n:])
@@ -34,11 +35,12 @@ class network:
                     if node_set.loc[k, 'Distance'] > dist:
                         node_set.loc[k,'Distance'] = dist
                         node_set.loc[k, 'Previous'] = current
-            shortestpath_mat.iloc[n:,n]=node_set.loc[:,'Distance']
-            shortestpath_mat.iloc[n, n:]=node_set.loc[:,'Distance']
+            shortestpath_df.iloc[n:,n]=node_set.loc[:,'Distance']
+            shortestpath_df.iloc[n, n:]=node_set.loc[:,'Distance']
             counter += 1
-        return shortestpath_mat
-    def sum_triangles(self):
+        return shortestpath_df
+
+    def num_triangles(self):
         triangles=pd.Series(np.zeros(len(self.nodes)), index=self.nodes)
         all_combinations=combinations(self.nodes, 3)    # Create list of all possible triangles
         abs_adj_mat = self.adj_mat.abs()
@@ -47,7 +49,19 @@ class network:
             n1_n2=abs_adj_mat.loc[comb[0],comb[1]]
             n1_n3=abs_adj_mat.loc[comb[0],comb[2]]
             n2_n3=abs_adj_mat.loc[comb[1],comb[2]]
-            sum_dict[comb]=(n1_n2+n1_n3+n2_n3)**(1/3)   # Calculate the triangle sum of the combination
+            sum_dict[comb]=(n1_n2+n1_n3+n2_n3)**(1/3)   # Calculate the triangle sum of the combination and save it in dictionary
         for node in self.nodes:
             triangles[node]=0.5*np.sum([sum_dict[s] for s in sum_dict if node in s])    # Sum all of the triangles that contain the node
         return triangles
+
+    def char_path(self):
+        sum_shrtpath_df=self.shortestpath().sum(axis=1)     # Sums Shortest Path Dataframe along axis 1
+        avg_shrtpath=np.divide(sum_shrtpath_df, len(self.nodes)-1)  # Divide each element in sum array by n-1 regions
+        return np.sum(avg_shrtpath)/len(self.nodes)                 # Calculate sum of the sum array and take the average
+
+    def glob_efficiency(self):
+        inv_shrtpath=self.shortestpath().pow(-1)                    # Takes the inverse of each element of the Dataframe
+        for n in self.nodes: inv_shrtpath.loc[n,n]=0                # Set Diagonal from inf -> 0
+        sum_invpath_df=inv_shrtpath.sum(axis=1)                     # Sums Shortest Path Dataframe along axis 1
+        avg_invpath=np.divide(sum_invpath_df, len(self.nodes)-1)    # Divide each element in sum array by n-1 regions
+        return np.sum(avg_invpath)/len(self.nodes)                 # Calculate sum of the sum array and take the average
