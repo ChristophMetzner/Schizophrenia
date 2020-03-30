c638bd835ad86ca55c30e0f372217d3263fc8812
Fabian <fabian.kamp@fu-berlin.de>
2020-03-30 13:13:25 +0200

    Renamed Graph-Measures

---
+++ graph_measures/random_reference.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+
+def Heischberger(tc):
+    return C
+def 
+
+class random_net:
+    """
+    Contains methods to generate a random reference network that is matched to the input network,
+    which can later be used for further graph analysis
+    """
+    def __init__(self, timecourse, method):
+        """
+        :param timecourse: n x p dimenisional array
+        :param method: Rewire or Heischberger
+        """
+        if not isinstance(timecourse, (np.ndarray, pd.DataFrame):
+            raise ValueError('Timecourse must be in array like format')
+        self.timecourse=timecourse
+        self.
+    def
